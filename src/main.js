7fdf8b8428fdae72b003d2aa2c95eb1a5eda0d6b
randyflores91 <randyflores@Randys-MBP.fios-router.home>
2021-05-23 17:43:47 -0400

    Add Vuetify and the base code for the Calendar

--- src/main.js
+++ src/main.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
+import VueTextareAutosize from 'vue-textarea-autosize' 
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+
+Vue.use(VueTextareAutosize);
 
 Vue.config.productionTip = false
 
+var firebaseConfig = {
+  apiKey: "AIzaSyCz5UYKDcWv7anN2pf51NQ6TMMndNjcPCA",
+  authDomain: "calendar-e6ec9.firebaseapp.com",
+  databaseURL: "https://calendar-e6ec9-default-rtdb.firebaseio.com",
+  projectId: "calendar-e6ec9",
+  storageBucket: "calendar-e6ec9.appspot.com",
+  messagingSenderId: "514786467179",
+  appId: "1:514786467179:web:8edf1f1724be3882b9402e"
+};
+firebase.initializeApp(firebaseConfig);
+
+export const database = firebase.firestore();
+
 new Vue({
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
